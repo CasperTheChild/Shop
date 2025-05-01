5fd4b19ebdc324e6bf9a43e14c0dc6d1917201b7
TempName460 <asuslaptopacc7@gmail.com>
2025-05-01 23:48:37 +0600

    Initial Commit

---
+++ SportsStore/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models;
+
+namespace SportsStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IStoreRepository repository;
+
+        public HomeController(IStoreRepository repo)
+        {
+            repository = repo;
+        }
+
+        public ViewResult Index() => View(repository.Products);
+    }
+}
